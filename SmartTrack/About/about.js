52c0a74a7569579e423d0a2fba03f0877056e489
Ishan-K9868 <ishanksps@gmail.com>
2024-09-03 21:16:58 +0530

    Delete SmartTrack directory

--- SmartTrack/About/about.js
+++
@@ -1,4 +0,0 @@
-// Placeholder for any future JavaScript interactions
-// Currently, no specific JavaScript is required for this static page
-
-// You can add some interactive features like loading animations or more dynamic behavior if needed
